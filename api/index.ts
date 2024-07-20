27dbc758f8cda0191016c016da18eed45fe10a12
tigawanna <denniskinuthiaw@gmail.com>
2024-07-20 20:14:58 +0300

    combine

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,41 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
-app.get('/auth', (c) => {
-  return c.json({ message: 'Hello auth' })
-})
+app.post("/", async (c) => {
+  const { username, password } = await c.req.parseBody<{ username: string; password: string }>();
+  console.log(" ============= hono js ====================== ", username, password);
+  if (username && username === "") {
+    return c.json({ message: "username required" });
+  }
+  if (password && password === "") {
+    return c.json({ message: "password required" });
+  }
+  // change to your endpoint
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      // change to your headers
+      "Content-Type": "application/json",
+      "x-tenentid": "Schryver Pruebas",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  if (!res.ok) {
+    return c.json({ message: `Auth Error: ${res.status}` });
+  }
 
-export default handle(app)
+  const data = await res.json();
+  c.res.headers.append("Content-Type", "application/json");
+  return c.json({message:"login success",data}, 200);
+});
+
+export default handle(app);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import "./App.css";
 
 function App() {
-return (
+  return (
     <div className="w-full h-full">
       <h1>Hello</h1>
+      <button
+        className=""
+        onClick={() => [
+          fetch("/api", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: "test",
+              password: "test",
+            }),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log("===================== login response ====================",data);
+            })
+            .catch((err) => console.log("==================== login error ====================",err)),
+        ]}>
+        login
+      </button>
     </div>
   );
 }
